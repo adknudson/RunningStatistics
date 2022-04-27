c823ffb1ec76d7c68caecb76cc84d6595dae48a1
Alex Knudson <alexk.706@gmail.com>
2022-04-26 22:18:41 -0700

    Added internal NumBins property

--- RunningStatistics/EmpiricalCdf.cs
+++ RunningStatistics/EmpiricalCdf.cs
@@ -9,16 +9,15 @@ namespace RunningStatistics;
 /// </summary>
 public class EmpiricalCdf : IRunningStatistic<double>
 {
-    private readonly int _numBins;
     private readonly Extrema _extrema;
     private readonly double[] _buffer, _values;
 
 
     public EmpiricalCdf(int numBins = 200)
     {
-        _numBins = numBins;
-        _values = new double[_numBins];
-        _buffer = new double[_numBins];
+        NumBins = numBins;
+        _values = new double[NumBins];
+        _buffer = new double[NumBins];
         _extrema = new Extrema();
     }
 
@@ -27,6 +26,7 @@ public class EmpiricalCdf : IRunningStatistic<double>
     public double Median => Quantile(0.5);
     public double Min => _extrema.Min;
     public double Max => _extrema.Max;
+    internal int NumBins { get; }
 
 
     public double Quantile(double p) => SortedQuantile(p);
@@ -35,17 +35,17 @@ public class EmpiricalCdf : IRunningStatistic<double>
     {
         if (other is not EmpiricalCdf empiricalCdf) return;
         
-        if (_numBins != empiricalCdf._numBins)
+        if (NumBins != empiricalCdf.NumBins)
         {
             throw new Exception($"The two {nameof(EmpiricalCdf)} objects must have the same batch size. " +
-                                $"Got {_numBins} and {empiricalCdf._numBins}.");
+                                $"Got {NumBins} and {empiricalCdf.NumBins}.");
         }
 
         _extrema.Merge(empiricalCdf._extrema);
 
         if (Count == 0) return;
 
-        for (var k = 0; k < _numBins; k++)
+        for (var k = 0; k < NumBins; k++)
         {
             _values[k] = Utils.Smooth(_values[k], empiricalCdf._values[k], (double) empiricalCdf.Count / Count);
         }
@@ -61,24 +61,24 @@ public class EmpiricalCdf : IRunningStatistic<double>
 
     public void Fit(double value)
     {
-        var i = (int) (Count % _numBins);
+        var i = (int) (Count % NumBins);
         _extrema.Fit(value);
         _buffer[i] = value;
 
         // if the buffer is full, then merge buffer into value vector
-        if (i == _numBins - 1)
+        if (i == NumBins - 1)
         {
             Array.Sort(_buffer);
-            for (var k = 0; k < _numBins; k++)
+            for (var k = 0; k < NumBins; k++)
             {
-                _values[k] = Utils.Smooth(_values[k], _buffer[k], (double) _numBins / Count);
+                _values[k] = Utils.Smooth(_values[k], _buffer[k], (double) NumBins / Count);
             }
         }
     }
 
     public void Reset()
     {
-        for (var i = 0; i < _numBins; i++)
+        for (var i = 0; i < NumBins; i++)
         {
             _values[i] = 0;
             _buffer[i] = 0;
@@ -99,7 +99,7 @@ public class EmpiricalCdf : IRunningStatistic<double>
             throw new ArgumentOutOfRangeException($"p must be in range [0, 1]. Got {p}.");
         }
 
-        var i = (int) Math.Floor((_numBins - 1) * p);
+        var i = (int) Math.Floor((NumBins - 1) * p);
         return _values.ElementAt(i);
     }
 }
