d6c408a8c5e915d71f2dcb506a3d7964190fc918
Alex Knudson <alexk.706@gmail.com>
2023-04-12 15:31:01 -0700

    Added examples project

---
+++ examples/RunningStatistics.Examples/StatsCollection.cs
@@ -0,0 +1,48 @@
+﻿namespace RunningStatistics.Examples;
+
+public class StatsCollection : AbstractRunningStatistic<double, StatsCollection>
+{
+    public StatsCollection()
+    {
+        CountMap = new CountMap<double>();
+        Ecdf = new EmpiricalCdf();
+    }
+    
+    
+    
+    private CountMap<double> CountMap { get; init; }
+    private EmpiricalCdf Ecdf { get; init; }
+
+
+
+    protected override long GetNobs() => Ecdf.Nobs;
+
+    public override void Fit(double value)
+    {
+        CountMap.Fit(value);
+        Ecdf.Fit(value);
+    }
+
+    public override void Reset()
+    {
+        CountMap.Reset();
+        Ecdf.Reset();
+    }
+
+    public override StatsCollection CloneEmpty() => new();
+
+    public override StatsCollection Clone()
+    {
+        return new StatsCollection
+        {
+            CountMap = CountMap.Clone(),
+            Ecdf = Ecdf.Clone()
+        };
+    }
+
+    public override void Merge(StatsCollection other)
+    {
+        CountMap.Merge(other.CountMap);
+        Ecdf.Merge(other.Ecdf);
+    }
+}
