bd9b79a192f9a8dc963c9e65da86c63b19aea6f2
Alex Knudson <alexk.706@gmail.com>
2023-04-21 20:33:02 -0700

    Initial class for ExpandingHistogram and HistogramStepRange

---
+++ src/RunningStatistics/HistogramStepRange.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RunningStatistics;
+
+public class HistogramStepRange : IEnumerable<double>
+{
+    public HistogramStepRange(double start, double stop, int length)
+    {
+        if (length < 2)
+        {
+            throw new ArgumentException("Length must be greater than or equal to 2.", nameof(length));
+        }
+
+        if (start > stop)
+        {
+            throw new ArgumentException("The stop must be greater than or equal to the start.");
+        }
+        
+        Start = start;
+        Stop = stop;
+        Length = length;
+    }
+    
+    
+    
+    public double Start { get; set; }
+    
+    public double Stop { get; set; }
+    
+    public int Length { get; set; }
+
+    public double Step => (Stop - Start) / (Length - 1);
+
+    public double Range => Stop - Start;
+
+    
+    
+    public IEnumerator<double> GetEnumerator()
+    {
+        yield return Start;
+
+        if (Length > 2)
+        {
+            for (var i = 1; i < Length - 1; i++)
+            {
+                yield return Start + Range * i / (Length - 1);
+            }
+        }
+        
+        yield return Stop;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public override string ToString() => $"{Start}:{Step}:{Stop}";
+}
