ac3413cceac01bdf65807be97e5171a0e5c99c21
Alex Knudson <alexk.706@gmail.com>
2023-04-27 16:09:34 -0700

    Added Beta distribution running stat, and all associated functions
    Borrowed Special Functions from MathNET.Numerics

---
+++ src/RunningStatistics/Beta.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using RunningStatistics.RootFinding;
+
+namespace RunningStatistics;
+
+public class Beta : AbstractRunningStatistic<bool, Beta>
+{
+    private long _a, _b;
+
+
+
+    public Beta()
+    {
+        _a = 0;
+        _b = 0;
+    }
+
+    public Beta(long numHits, long numMisses)
+    {
+        _a = numHits;
+        _b = numMisses;
+    }
+
+
+
+    public double Mean => Nobs > 0 ? (double)_a / Nobs : double.NaN;
+
+    public double Median => _a > 1 && _b > 1 ? (_a - 0.33333333333333331) / (Nobs - 0.66666666666666663) : double.NaN;
+
+    public double Mode => ComputeMode();
+    
+    public double Variance => (double) _a * _b / (Nobs * Nobs * (Nobs + 1));
+
+    
+    
+    protected override long GetNobs() => _a + _b;
+    
+    public void Fit(long numHits = 0, long numMisses = 0)
+    {
+        if (numHits < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numHits), "Number of hits must be non-negative");
+        }
+
+        if (numMisses < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numMisses), "Number of misses must be non-negative");
+        }
+
+        UncheckedFit(numHits, numMisses);
+    }
+
+    public override void Fit(bool success)
+    {
+        if (success)
+        {
+            _a += 1;
+        }
+        else
+        {
+            _b += 1;
+        }
+    }
+
+    private void UncheckedFit(long numHits, long numMisses)
+    {
+        _a += numHits;
+        _b += numMisses;
+    }
+
+    public override void Reset()
+    {
+        _a = 0;
+        _b = 0;
+    }
+
+    public override Beta CloneEmpty() => new();
+
+    public override Beta Clone() => new(_a, _b);
+
+    public override void Merge(Beta other)
+    {
+        _a += other._a;
+        _b += other._b;
+    }
+
+    private double ComputeMode()
+    {
+        if (_a >  1 && _b >  1) return (double) (_a - 1) / (Nobs - 2);
+        if (_a == 1 && _b == 1) return double.NaN;
+        if (_a == 0 && _b  > 1) return 0.0;
+        if (_a >  1 && _b == 0) return 1.0;
+
+        return double.NaN;
+    }
+    
+    public double Pdf(double x)
+    {
+        if (x < 0.0 || x > 1.0)
+        {
+            return 0.0;
+        }
+            
+        // x is now in [0, 1]
+        
+        if (_a == 0 && _b == 0)
+        {
+            if (x == 0.0 || x == 1.0)
+            {
+                return double.PositiveInfinity;
+            }
+
+            return 0.0;
+        }
+
+        if (_a == 0)
+        {
+            return x == 0.0 ? double.PositiveInfinity : 0.0;
+        }
+
+        if (_b == 0)
+        {
+            return x == 1.0 ? double.PositiveInfinity : 0.0;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return 1.0;
+        }
+
+        if (_a > 80.0 || _b > 80.0)
+        {
+            return Math.Exp(PdfLn(x));
+        }
+
+        var bb = SpecialFunctions.Gamma(_a + _b) / (SpecialFunctions.Gamma(_a) * SpecialFunctions.Gamma(_b));
+        return bb * Math.Pow(x, _a - 1.0) * Math.Pow(1.0 - x, _b - 1.0);
+    }
+    
+    private double PdfLn(double x)
+    {
+        if (x < 0.0 || x > 1.0)
+        {
+            return double.NegativeInfinity;
+        }
+
+        if (_a == 0 && _b == 0)
+        {
+            return x == 0.0 || x == 1.0 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_a == 0)
+        {
+            return x == 0.0 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_b == 0)
+        {
+            return x == 1.0 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return 0.0;
+        }
+
+        var aa = SpecialFunctions.GammaLn(_a + _b) - SpecialFunctions.GammaLn(_a) - SpecialFunctions.GammaLn(_b);
+        var bb = x == 0.0 ? (_a == 1 ? 0.0 : double.NegativeInfinity) : (_a - 1.0) * Math.Log(x);
+        var cc = x == 1.0 ? (_b == 1 ? 0.0 : double.NegativeInfinity) : (_b - 1.0) * Math.Log(1.0 - x);
+
+        return aa + bb + cc;
+    }
+    
+    public double Cdf(double x)
+    {
+        if (x < 0.0)
+        {
+            return 0.0;
+        }
+
+        if (x >= 1.0)
+        {
+            return 1.0;
+        }
+            
+        // x is now in [0, 1)
+
+        if (_a == 0 && _b == 0)
+        {
+            return 0.5;
+        }
+
+        if (_a == 0)
+        {
+            return 1.0;
+        }
+
+        if (_b == 0)
+        {
+            return 0.0;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return x;
+        }
+
+        return SpecialFunctions.UnsafeBetaRegularized(_a, _b, x);
+    }
+
+    public double Quantile(double p)
+    {
+        if (p is < 0.0 or > 1.0)
+        {
+            throw new ArgumentException("Invalid parametrization for the distribution.");
+        } 
+        
+        return Brent.FindRoot(x => SpecialFunctions.UnsafeBetaRegularized(_a, _b, x) - p, 0.0, 1.0, accuracy: 1e-12);
+    }
+}
---
+++ src/RunningStatistics/RootFinding/Brent.cs
@@ -0,0 +1,175 @@
+﻿// <copyright file="Brent.cs" company="Math.NET">
+// Math.NET Numerics, part of the Math.NET Project
+// http://numerics.mathdotnet.com
+// http://github.com/mathnet/mathnet-numerics
+//
+// Copyright (c) 2009-2020 Math.NET
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+using System;
+
+namespace RunningStatistics.RootFinding;
+
+internal static class Brent
+{
+    public static double FindRoot(Func<double, double> f, double lowerBound, double upperBound, double accuracy = 1e-8,
+        int maxIterations = 100)
+    {
+        if (TryFindRoot(f, lowerBound, upperBound, accuracy, maxIterations, out var root))
+        {
+            return root;
+        }
+
+        throw new Exception(
+            "The algorithm has failed, exceeded the number of iterations allowed or there is no root within the provided bounds.");
+    }
+
+    private static bool TryFindRoot(Func<double, double> f, double lowerBound, double upperBound, double accuracy,
+        int maxIterations, out double root)
+    {
+        if (accuracy <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(accuracy), "Must be greater than zero.");
+        }
+
+        var fMin = f(lowerBound);
+        var fMax = f(upperBound);
+        var fRoot = fMax;
+        double d = 0.0, e = 0.0;
+
+        root = upperBound;
+        var xMid = double.NaN;
+
+        // Root must be bracketed.
+        if (Math.Sign(fMin) == Math.Sign(fMax))
+        {
+            return false;
+        }
+
+        for (var i = 0; i <= maxIterations; i++)
+        {
+            // adjust bounds
+            if (Math.Sign(fRoot) == Math.Sign(fMax))
+            {
+                upperBound = lowerBound;
+                fMax = fMin;
+                e = d = root - lowerBound;
+            }
+
+            if (Math.Abs(fMax) < Math.Abs(fRoot))
+            {
+                lowerBound = root;
+                root = upperBound;
+                upperBound = lowerBound;
+                fMin = fRoot;
+                fRoot = fMax;
+                fMax = fMin;
+            }
+
+            // convergence check
+            var xAcc1 = Precision.PositiveDoublePrecision * Math.Abs(root) + 0.5 * accuracy;
+            var xMidOld = xMid;
+            xMid = (upperBound - root) / 2.0;
+
+            if (Math.Abs(xMid) <= xAcc1 || fRoot.AlmostEqualNormRelative(0, fRoot, accuracy))
+            {
+                return true;
+            }
+
+            if (xMid == xMidOld)
+            {
+                // accuracy not sufficient, but cannot be improved further
+                return false;
+            }
+
+            if (Math.Abs(e) >= xAcc1 && Math.Abs(fMin) > Math.Abs(fRoot))
+            {
+                // Attempt inverse quadratic interpolation
+                var s = fRoot / fMin;
+                double p;
+                double q;
+                if (lowerBound.AlmostEqualRelative(upperBound))
+                {
+                    p = 2.0 * xMid * s;
+                    q = 1.0 - s;
+                }
+                else
+                {
+                    q = fMin / fMax;
+                    var r = fRoot / fMax;
+                    p = s * (2.0 * xMid * q * (q - r) - (root - lowerBound) * (r - 1.0));
+                    q = (q - 1.0) * (r - 1.0) * (s - 1.0);
+                }
+
+                if (p > 0.0)
+                {
+                    // Check whether in bounds
+                    q = -q;
+                }
+
+                p = Math.Abs(p);
+                if (2.0 * p < Math.Min(3.0 * xMid * q - Math.Abs(xAcc1 * q), Math.Abs(e * q)))
+                {
+                    // Accept interpolation
+                    e = d;
+                    d = p / q;
+                }
+                else
+                {
+                    // Interpolation failed, use bisection
+                    d = xMid;
+                    e = d;
+                }
+            }
+            else
+            {
+                // Bounds decreasing too slowly, use bisection
+                d = xMid;
+                e = d;
+            }
+
+            lowerBound = root;
+            fMin = fRoot;
+            if (Math.Abs(d) > xAcc1)
+            {
+                root += d;
+            }
+            else
+            {
+                root += Sign(xAcc1, xMid);
+            }
+            
+            fRoot = f(root);
+        }
+
+        return false;
+    }
+
+    /// <summary>Helper method useful for preventing rounding errors.</summary>
+    /// <returns>a*sign(b)</returns>
+    private static double Sign(double a, double b)
+    {
+        return b >= 0 ? a >= 0 ? a : -a : a >= 0 ? -a : a;
+    }
+}
