57f8afb9b1598e86676ef1e2fce44c631a13dd5b
adknudson <alexk.706@gmail.com>
2024-12-28 10:08:25 -0800

    Add UnsafeBeta.cs and make Beta use it internally

--- src/RunningStatistics/Statistics/Beta.cs
+++ src/RunningStatistics/Statistics/Beta.cs
@@ -1,251 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using RunningStatistics.UnsafeStatistics;
 
 // ReSharper disable CompareOfFloatsByEqualityOperator
 // ReSharper disable ConvertToAutoPropertyWithPrivateSetter
 
 namespace RunningStatistics;
 
-public sealed class Beta : RunningStatisticBase<bool, Beta>
+public sealed class Beta : IRunningStatistic<bool, Beta>
 {
-    private long _a, _b;
+    private readonly UnsafeBeta _beta = new();
 
-    
+
     public Beta()
     {
-        _a = 0;
-        _b = 0;
-    }
-
-    public Beta(long numSuccesses, long numFailures)
-    {
-        if (numSuccesses < 0)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(numSuccesses), numSuccesses, "Number of successes must be non-negative");
-        }
-
-        if (numFailures < 0)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(numFailures), numFailures, "Number of failures must be non-negative");
-        }
-        
-        _a = numSuccesses;
-        _b = numFailures;
     }
     
+    public Beta(long successes, long failures)
+    {
+        Require.NonNegative(successes);
+        Require.NonNegative(failures);
+        _beta.Fit(successes, failures);
+    }
 
-    public long Successes => _a;
 
-    public long Failures => _b;
+    public long Nobs => _beta.Nobs;
     
-    public double Mean => Nobs > 0 ? (double)_a / Nobs : double.NaN;
+    public long Successes => _beta.Successes;
 
-    public double Median => _a > 1 && _b > 1 ? (_a - 0.33333333333333331) / (Nobs - 0.66666666666666663) : double.NaN;
+    public long Failures => _beta.Failures;
 
-    public double Mode => ComputeMode();
+    public double Mean => Nobs == 0 ? double.NaN : _beta.Mean;
+
+    public double Median => _beta.Median;
+
+    public double Mode => _beta.Mode;
+
+    public double Variance => _beta.Variance;
+
+
+    public void Fit(bool success)
+    {
+        _beta.Fit(success);
+    }
     
-    public double Variance => (double) _a * _b / (Nobs * Nobs * (Nobs + 1));
-
-
-    protected override long GetNobs() => _a + _b;
-
-    public void Fit(long numSuccesses, long numFailures)
+    public void Fit(long successes, long failures)
     {
-        if (numSuccesses < 0)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(numSuccesses), numSuccesses, "Number of successes must be non-negative");
-        }
-
-        if (numFailures < 0)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(numFailures), numFailures, "Number of failures must be non-negative");
-        }
-
-        UncheckedFit(numSuccesses, numFailures);
+        Require.NonNegative(successes);
+        Require.NonNegative(failures);
+        _beta.Fit(successes, failures);
     }
 
-    public override void Fit(bool success)
+    public void Fit(IEnumerable<bool> values)
     {
-        if (success)
-        {
-            _a += 1;
-        }
-        else
-        {
-            _b += 1;
-        }
+        _beta.Fit(values);
     }
 
-    public override void Fit(IEnumerable<bool> values)
+    public void Reset()
     {
-        var bs = values.ToList();
-        var k = bs.Count(b => b);
-        UncheckedFit(k, bs.Count - k);
+        _beta.Reset();
     }
 
-    private void UncheckedFit(long numSuccesses, long numFailures)
+    IRunningStatistic<bool> IRunningStatistic<bool>.CloneEmpty()
     {
-        _a += numSuccesses;
-        _b += numFailures;
+        return CloneEmpty();
     }
 
-    public override void Reset()
+    IRunningStatistic<bool> IRunningStatistic<bool>.Clone()
     {
-        _a = 0;
-        _b = 0;
+        return Clone();
     }
 
-    public override Beta CloneEmpty() => new();
-
-    public override Beta Clone() => new(_a, _b);
-
-    public override void Merge(Beta other)
+    public void Merge(IRunningStatistic<bool> other)
     {
-        _a += other._a;
-        _b += other._b;
+        var beta = Require.Type<Beta>(other);
+        Merge(beta);
     }
 
-    private double ComputeMode()
-    {
-        if (_a >  1 && _b >  1) return (double) (_a - 1) / (Nobs - 2);
-        if (_a == 1 && _b == 1) return double.NaN;
-        if (_a == 0 && _b  > 1) return 0.0;
-        if (_a >  1 && _b == 0) return 1.0;
+    public Beta CloneEmpty() => new();
 
-        return double.NaN;
+    public Beta Clone() => new(Successes, Failures);
+
+    public void Merge(Beta other)
+    {
+        _beta.Merge(other._beta);
     }
     
     public double Pdf(double x)
     {
-        if (x is < 0 or > 1)
-        {
-            return 0.0;
-        }
-            
-        // x is now in [0, 1]
-        
-        if (_a == 0 && _b == 0)
-        {
-            if (x is 0 or 1)
-            {
-                return double.PositiveInfinity;
-            }
-
-            return 0.0;
-        }
-
-        if (_a == 0)
-        {
-            return x == 0 ? double.PositiveInfinity : 0;
-        }
-
-        if (_b == 0)
-        {
-            return x == 1 ? double.PositiveInfinity : 0;
-        }
-
-        if (_a == 1 && _b == 1)
-        {
-            return 1;
-        }
-
-        if (_a > 80 || _b > 80)
-        {
-            return Math.Exp(PdfLn(x));
-        }
-
-        var bb = SpecialFunctions.Gamma(_a + _b) / (SpecialFunctions.Gamma(_a) * SpecialFunctions.Gamma(_b));
-        return bb * Math.Pow(x, _a - 1.0) * Math.Pow(1.0 - x, _b - 1.0);
+        return _beta.Pdf(x);
     }
-    
-    private double PdfLn(double x)
-    {
-        if (x is < 0 or > 1)
-        {
-            return double.NegativeInfinity;
-        }
 
-        if (_a == 0 && _b == 0)
-        {
-            return x is 0 or 1 ? double.PositiveInfinity : double.NegativeInfinity;
-        }
-
-        if (_a == 0)
-        {
-            return x == 0 ? double.PositiveInfinity : double.NegativeInfinity;
-        }
-
-        if (_b == 0)
-        {
-            return x == 1 ? double.PositiveInfinity : double.NegativeInfinity;
-        }
-
-        if (_a == 1 && _b == 1)
-        {
-            return 0;
-        }
-
-        var aa = SpecialFunctions.GammaLn(_a + _b) - SpecialFunctions.GammaLn(_a) - SpecialFunctions.GammaLn(_b);
-        var bb = x == 0.0 ? (_a == 1 ? 0.0 : double.NegativeInfinity) : (_a - 1.0) * Math.Log(x);
-        var cc = x == 1.0 ? (_b == 1 ? 0.0 : double.NegativeInfinity) : (_b - 1.0) * Math.Log(1.0 - x);
-
-        return aa + bb + cc;
-    }
-    
     public double Cdf(double x)
     {
-        if (_a == 0 && _b == 0)
+        if (Successes == 0 && Failures == 0)
         {
-            throw new ArgumentException("Undefined for both a = 0 and b = 0"); 
-        }
-        
-        if (x < 0)
-        {
-            return 0;
+            throw new ArgumentException("CDF is undefined for both a = 0 and b = 0"); 
         }
 
-        if (x >= 1)
-        {
-            return 1;
-        }
-            
-        // x is now in [0, 1)
-        
-        if (_a == 0)
-        {
-            return 1;
-        }
-
-        if (_b == 0)
-        {
-            return 0;
-        }
-
-        if (_a == 1 && _b == 1)
-        {
-            return x;
-        }
-
-        return SpecialFunctions.UnsafeBetaRegularized(_a, _b, x);
+        return _beta.Cdf(x);
     }
 
     public double Quantile(double p)
     {
-        if (p is < 0 or > 1)
-        {
-            throw new ArgumentOutOfRangeException(
-                nameof(p), p, "Quantile value must be in the interval [0, 1]");
-        }
-
-        return RootFinding.FindRoot(
-            x => SpecialFunctions.UnsafeBetaRegularized(_a, _b, x) - p,
-            0.0,
-            1.0,
-            accuracy: 1e-12);
+        Require.ValidProbability(p);
+        return _beta.Quantile(p);
     }
 }
---
+++ src/RunningStatistics/UnsafeStatistics/UnsafeBeta.cs
@@ -0,0 +1,225 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable ConvertToAutoPropertyWithPrivateSetter
+// ReSharper disable ConvertIfStatementToSwitchStatement
+// ReSharper disable CompareOfFloatsByEqualityOperator
+
+namespace RunningStatistics.UnsafeStatistics;
+
+public sealed class UnsafeBeta : IRunningStatistic<bool, UnsafeBeta>
+{
+    private long _a, _b;
+    
+    
+    public long Nobs => Successes + Failures;
+
+    public long Successes => _a;
+
+    public long Failures => _b;
+
+    public double Mean => (double)Successes / Nobs;
+
+    public double Median => _a > 1 && _b > 1 ? (_a - 0.33333333333333331) / (Nobs - 0.66666666666666663) : double.NaN;
+    
+    public double Mode
+    {
+        get
+        {
+            if (_a >  1 && _b >  1) return (double) (_a - 1) / (Nobs - 2);
+            if (_a == 0 && _b  > 1) return 0.0;
+            if (_a >  1 && _b == 0) return 1.0;
+
+            return double.NaN;
+        }
+    }
+    
+    public double Variance => (double) _a * _b / (Nobs * Nobs * (Nobs + 1));
+    
+    
+    public void Fit(bool value)
+    {
+        if (value)
+        {
+            _a += 1;
+        }
+        else
+        {
+            _b += 1;
+        }
+    }
+
+    public void Fit(long successes, long failures)
+    {
+        _a += successes;
+        _b += failures;
+    }
+
+    public void Fit(IEnumerable<bool> values)
+    {
+        var bs = values.ToList();
+        var n = bs.Count;
+        var s = bs.Count(b => b);
+        Fit(s, n - s);
+    }
+
+    public void Reset()
+    {
+        _a = 0;
+        _b = 0;
+    }
+
+    public UnsafeBeta CloneEmpty()
+    {
+        return new UnsafeBeta();
+    }
+
+    public UnsafeBeta Clone()
+    {
+        return new UnsafeBeta
+        {
+            _a = Successes,
+            _b = Failures
+        };
+    }
+
+    public void Merge(UnsafeBeta other)
+    {
+        _a += other.Successes;
+        _b += other.Failures;
+    }
+
+    IRunningStatistic<bool> IRunningStatistic<bool>.CloneEmpty()
+    {
+        return CloneEmpty();
+    }
+
+    IRunningStatistic<bool> IRunningStatistic<bool>.Clone()
+    {
+        return Clone();
+    }
+
+    public void Merge(IRunningStatistic<bool> other)
+    {
+        Merge((UnsafeBeta)other);
+    }
+    
+    public double Pdf(double x)
+    {
+        if (x is < 0 or > 1)
+        {
+            return 0.0;
+        }
+            
+        // x is now in [0, 1]
+        
+        if (_a == 0 && _b == 0)
+        {
+            if (x is 0 or 1)
+            {
+                return double.PositiveInfinity;
+            }
+
+            return 0.0;
+        }
+
+        if (_a == 0)
+        {
+            return x == 0 ? double.PositiveInfinity : 0;
+        }
+
+        if (_b == 0)
+        {
+            return x == 1 ? double.PositiveInfinity : 0;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return 1;
+        }
+
+        if (_a > 80 || _b > 80)
+        {
+            return Math.Exp(PdfLn(x));
+        }
+
+        var bb = SpecialFunctions.Gamma(_a + _b) / (SpecialFunctions.Gamma(_a) * SpecialFunctions.Gamma(_b));
+        return bb * Math.Pow(x, _a - 1.0) * Math.Pow(1.0 - x, _b - 1.0);
+    }
+    
+    private double PdfLn(double x)
+    {
+        if (x is < 0 or > 1)
+        {
+            return double.NegativeInfinity;
+        }
+
+        if (_a == 0 && _b == 0)
+        {
+            return x is 0 or 1 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_a == 0)
+        {
+            return x == 0 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_b == 0)
+        {
+            return x == 1 ? double.PositiveInfinity : double.NegativeInfinity;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return 0;
+        }
+
+        var aa = SpecialFunctions.GammaLn(_a + _b) - SpecialFunctions.GammaLn(_a) - SpecialFunctions.GammaLn(_b);
+        var bb = x == 0.0 ? (_a == 1 ? 0.0 : double.NegativeInfinity) : (_a - 1.0) * Math.Log(x);
+        var cc = x == 1.0 ? (_b == 1 ? 0.0 : double.NegativeInfinity) : (_b - 1.0) * Math.Log(1.0 - x);
+
+        return aa + bb + cc;
+    }
+    
+    public double Cdf(double x)
+    {
+        if (x < 0)
+        {
+            return 0;
+        }
+
+        if (x >= 1)
+        {
+            return 1;
+        }
+            
+        // x is now in [0, 1)
+        
+        if (_a == 0)
+        {
+            return 1;
+        }
+
+        if (_b == 0)
+        {
+            return 0;
+        }
+
+        if (_a == 1 && _b == 1)
+        {
+            return x;
+        }
+
+        return SpecialFunctions.UnsafeBetaRegularized(_a, _b, x);
+    }
+
+    public double Quantile(double p)
+    {
+        return RootFinding.FindRoot(
+            x => SpecialFunctions.UnsafeBetaRegularized(Successes, Failures, x) - p,
+            0.0,
+            1.0,
+            accuracy: 1e-12);
+    }
+}
