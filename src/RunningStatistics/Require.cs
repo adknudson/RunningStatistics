47edee50345cd95f5c4a18d1133191b89e095995
adknudson <alexk.706@gmail.com>
2024-12-28 10:06:13 -0800

    Added Require.Type<T> method to check if an object is a particular type.

--- src/RunningStatistics/Require.cs
+++ src/RunningStatistics/Require.cs
@@ -20,6 +20,25 @@ internal static class Require
         }
     }
 
+    public static void ValidProbability(double p)
+    {
+        if (p is < 0 or > 1)
+        {
+            throw new ArgumentOutOfRangeException(
+                nameof(p), p, "Value must be in the interval [0, 1]");
+        }
+    }
+
+    public static T Type<T>(object? obj)
+    {
+        if (obj is not T typed)
+        {
+            throw new ArgumentException($"The other stat must be of type {typeof(T)}", nameof(obj));
+        }
+
+        return typed;
+    }
+
     public static void NotNaN(double value)
     {
         if (double.IsNaN(value))
