b81544d69ba0a4a372fe6a19a995aeb0b8c1da55
adknudson <alexk.706@gmail.com>
2024-12-30 12:32:50 -0800

    Updated Extrema extension class

--- src/RunningStatistics/Extensions/ExtremaExtensions.cs
+++ src/RunningStatistics/Extensions/ExtremaExtensions.cs
@@ -1,16 +1,13 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace RunningStatistics;
 
 public static class ExtremaExtensions
 {
-#if NET7_0_OR_GREATER
-    
     public static T Range<T>(this Extrema<T> extrema) 
-        where T : IMinMaxValue<T>, IComparisonOperators<T, T, bool>, ISubtractionOperators<T, T, T>
+        where T : IMinMaxValue<T>, IComparable<T>, ISubtractionOperators<T, T, T>
     {
         return extrema.Max - extrema.Min;
     }
-    
-#endif
 }
