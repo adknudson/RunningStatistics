2c440226d1d552b49e99263b9b6d116f236739e3
adknudson <alexk.706@gmail.com>
2024-12-30 17:36:49 -0800

    Add example for ComplexStat and StatsCollection

---
+++ examples/RunningStatistics.Examples/ComplexStat.cs
@@ -0,0 +1,44 @@
+﻿namespace RunningStatistics.Examples;
+
+/// <summary>
+/// Example of a complex statistic that contains multiple other statistics.
+/// </summary>
+public class ComplexStat : RunningStatisticBase<double, ComplexStat>
+{
+    private Mean Mean { get; } = new();
+    
+    private EmpiricalCdf Cdf { get; } = new();
+    
+    private CountMap<double> CountMap { get; } = new();
+    
+    
+    protected override long GetNobs() => Mean.Nobs;
+
+    public override void Fit(double value, long count)
+    {
+        Mean.Fit(value, count);
+        Cdf.Fit(value, count);
+        CountMap.Fit(value, count);
+    }
+
+    public override void Reset()
+    {
+        Mean.Reset();
+        Cdf.Reset();
+        CountMap.Reset();
+    }
+
+    public override ComplexStat CloneEmpty() => new();
+
+    public override void Merge(ComplexStat other)
+    {
+        Mean.Merge(other.Mean);
+        Cdf.Merge(other.Cdf);
+        CountMap.Merge(other.CountMap);
+    }
+
+    protected override string GetStatsString()
+    {
+        return $"Mean: {Mean}, Cdf: {Cdf}, CountMap: {CountMap}";
+    }
+}
