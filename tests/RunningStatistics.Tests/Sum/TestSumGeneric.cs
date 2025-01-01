79053e2f486f605601c0326e7b67f0a76c9e37f6
adknudson <alexk.706@gmail.com>
2025-01-01 02:08:24 -0800

    add constructor for TestSumGeneric

--- tests/RunningStatistics.Tests/Sum/TestSumGeneric.cs
+++ tests/RunningStatistics.Tests/Sum/TestSumGeneric.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace RunningStatistics.Tests.Sum;
 
-public partial class TestSumGeneric : AbstractRunningStatsTest<double, Sum<double>>
-{
-    
-}
+public partial class TestSumGeneric() 
+    : AbstractRunningStatsTest<double, Sum<double>>(
+        () => new Sum<double>(),
+        () => Random.Shared.NextDouble());
