e2b49e816dee4509f85449addab8816ebeb04dc2
adknudson <alexk.706@gmail.com>
2025-01-03 14:02:07 -0800

    added doc strings for CountMap.cs

--- RunningStatistics/Statistics/CountMap.cs
+++ RunningStatistics/Statistics/CountMap.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace RunningStatistics;
 
+/// <summary>
+/// Represents a count map that tracks the number of occurrences of each unique observation.
+/// Accessing a key that has not been fitted will return zero, but the key will not be added to the
+/// dictionary. Fitting a key with a count of zero will also not add the key to the dictionary.
+/// </summary>
+/// <typeparam name="TObs">The observation type.</typeparam>
 public sealed class CountMap<TObs> : RunningStatisticBase<TObs, CountMap<TObs>>, IReadOnlyDictionary<TObs, long> 
     where TObs : notnull
 {
@@ -10,15 +16,27 @@ public sealed class CountMap<TObs> : RunningStatisticBase<TObs, CountMap<TObs>>,
     private long _nobs;
     
     
+    /// <summary>
+    /// Gets the count of the specified key. If the key has not been fitted, zero is returned.
+    /// </summary>
+    /// <param name="key">The key whose count to get.</param>
+    /// <returns>The count of the specified key.</returns>
     public long this[TObs key] => _dict.GetValueOrDefault(key, 0);
     
     /// <summary>
-    /// The number of unique observations that have been fitted. Observations with a count of zero are not included.
+    /// The number of unique observations that have been fitted. Observations with a count of zero
+    /// are not included.
     /// </summary>
     public int Count => _dict.Count;
 
+    /// <summary>
+    /// Gets an enumerable collection of the keys in the count map.
+    /// </summary>
     public IEnumerable<TObs> Keys => _dict.Keys;
 
+    /// <summary>
+    /// Gets an enumerable collection of the values in the count map.
+    /// </summary>
     public IEnumerable<long> Values => _dict.Values;
 
 
@@ -40,19 +58,19 @@ public sealed class CountMap<TObs> : RunningStatisticBase<TObs, CountMap<TObs>>,
             _dict[value] += count;
         }
     }
-
+    
     public override void Merge(CountMap<TObs> countMap) => Fit(countMap);
-
+    
     public override void Reset()
     {
         _dict.Clear();
         _nobs = 0;
     }
-
+    
     public override CountMap<TObs> CloneEmpty() => new();
     
     public bool ContainsKey(TObs key) => _dict.ContainsKey(key);
-
+    
     public bool TryGetValue(TObs key, out long value) => _dict.TryGetValue(key, out value);
     
     public IEnumerator<KeyValuePair<TObs, long>> GetEnumerator() => _dict.GetEnumerator();
