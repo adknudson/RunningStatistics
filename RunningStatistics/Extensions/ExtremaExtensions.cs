a8e889ad554e24fe651f815adc30f7f5d181cb30
adknudson <alexk.706@gmail.com>
2025-01-03 12:25:09 -0800

    added Range method for regular Extrema

--- RunningStatistics/Extensions/ExtremaExtensions.cs
+++ RunningStatistics/Extensions/ExtremaExtensions.cs
@@ -5,6 +5,11 @@ namespace RunningStatistics;
 
 public static class ExtremaExtensions
 {
+    public static double Range(this Extrema extrema)
+    {
+        return extrema.Max - extrema.Min;
+    }
+    
     public static T Range<T>(this Extrema<T> extrema) 
         where T : IMinMaxValue<T>, IComparable<T>, ISubtractionOperators<T, T, T>
     {
